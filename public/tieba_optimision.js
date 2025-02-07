2a80f00ef7e3ef6692566065893e375a1533dffe
caiqichang <cai_qichang@qq.com>
2025-02-07 15:33:49 +0800

    add zhihu optimision; update tieba optimision

--- public/tieba_optimision.js
+++ public/tieba_optimision.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         tieba_optimision
 // @namespace    http://tampermonkey.net/
-// @version      2025-01-21_10_02
+// @version      2025-02-07_15_07
 // @downloadURL  https://raw.githubusercontent.com/caiqichang/tampermonkey_scripts/refs/heads/master/public/tieba_optimision.js
 // @updateURL    https://raw.githubusercontent.com/caiqichang/tampermonkey_scripts/refs/heads/master/public/tieba_optimision.js
 // @description  tieba optimision
@@ -13,20 +13,33 @@
 (function () {
     'use strict';
 
-    document.querySelector(".r-top-sec")?.remove()
-    document.querySelector(".r-right-sec")?.remove()
-    document.querySelector(".left-sec")?.remove()
-    document.querySelector(".right_section.right_bright")?.remove()
-    document.querySelectorAll("div[ad-dom-img]")?.forEach(e => e.remove())
-    document.querySelectorAll("a[rel=\"noreferrer\"]")?.forEach(e => e.remove())
-    
     GM_addElement("style", {
         textContent: `
+            .r-top-sec,
+            .r-right-sec,
+            .left-sec,
+            .right_section.right_bright,
+            div[ad-dom-img],
+            .forum_content .aside,
+            .card_banner.card_banner_link,
+            .card_top_wrap.clearfix.card_top_theme,
+            .j_click_stats,
+            .clearfix.thread_item_box:not(.j_thread_list),
+            .l_post.l_post_bright.j_l_post.clearfix[data-field="{}"]
+            {
+                display: none;
+            }
             .top-sec.clearfix {
                 opacity: 0;
             }
             .page-container .content-sec {
                 background: unset;
+            }
+            .search_internal_wrap {
+                float: left !important;
+            }
+            .nav_list.j_nav_list {
+                width: unset;
             }
         `,
     })
---
+++ public/zhihu_optimision.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         zhihu_optimision
+// @namespace    http://tampermonkey.net/
+// @version      2025-02-07_14_13
+// @downloadURL  https://raw.githubusercontent.com/caiqichang/tampermonkey_scripts/refs/heads/master/public/zhihu_optimision.js
+// @updateURL    https://raw.githubusercontent.com/caiqichang/tampermonkey_scripts/refs/heads/master/public/zhihu_optimision.js
+// @description  zhihu optimision
+// @author       caiqichang
+// @match        *://*.zhihu.com/*
+// @grant        GM_addElement
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    document.querySelector("#SearchMain")?.nextSibling?.remove()
+    
+    GM_addElement("style", {
+        textContent: `
+            .AppHeader-inner {
+                min-width: unset;
+                width: 800px;
+                margin-left: 0px;
+            }
+            .AppHeader.is-hidden .AppHeader-inner {
+                -webkit-transform: unset !important;
+                transform: unset !important;
+            }
+            .PageHeader.is-shown,
+            .SearchBar-label:not(.SearchBar-label--history),
+            [data-za-module=HotSearchWordList],
+            .Pc-Business-Card-PcTopFeedBanner,
+            .Tabs-item.AppHeader-Tab.Tabs-item--noMeta,
+            [data-za-detail-view-path-module=RightSideBar]
+            {
+                display: none;
+            }
+        `,
+    })
+})();
